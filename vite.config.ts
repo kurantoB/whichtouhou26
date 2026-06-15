14bbb635fd848a55ce82916c84913f9c3fb47b7f
kurantoB <laomeister93@gmail.com>
2026-06-15 08:14:54 -0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/whichtouhou26/',
   plugins: [vue(), tailwindcss()],
 })
